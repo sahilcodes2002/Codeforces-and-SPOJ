39e1e0efd7670dd4494793afadc5e0fd605d9fc0
sahilcodes2002 <114796210+sahilcodes2002@users.noreply.github.com>
2024-01-24 02:59:26 +0530

    Create two pointer codeforces C. Number of Equal.cpp

---
+++ two pointer codeforces C. Number of Equal.cpp
@@ -0,0 +1,35 @@
+//https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/C
+
+
+// C. Number of Equal
+// time limit per test1 second
+// memory limit per test256 megabytes
+// inputstandard input
+// outputstandard output
+// You are given two arrays a
+//  and b
+// , sorted in non-decreasing order. Find the number of pairs (i,j)
+//  for which ai=bj
+// .
+
+// Input
+// The first line contains integers n
+//  and m
+// , the sizes of the arrays (1≤n,m≤105
+// ). The second line contains n
+//  integers ai
+// , elements of the first array, the third line contains m
+//  integers bi
+// , elements of the second array (−109≤ai,bi≤109
+// ).
+
+// Output
+// Print one number, the answer to the problem.
+
+// Example
+// inputCopy
+// 8 7
+// 1 1 3 3 3 5 8 8
+// 1 3 3 4 5 5 5
+// outputCopy
+// 11
